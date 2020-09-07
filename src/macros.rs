a58eec97bee9acc94b6e900f00e21f206982ef7a
poyeker <poyeker@gmail.com>
2020-09-07 20:59:56 +0800

    seed changes

--- src/macros.rs
+++ src/macros.rs
@@ -1,11 +1,11 @@
 #[macro_export]
 macro_rules! make_rng {
     () => {{
-        let mut r = Rand::new(Pcg64Mcg::from_entropy());
+        let mut r = Rand::new(Pcg64Mcg::from_rng(thread_rng()).unwrap());
         r
     }};
     ($rng:ident) => {{
-        let mut r = Rand::new($rng::from_entropy());
+        let mut r = Rand::new($rng::from_rng(thread_rng()).unwrap());
         r
     }};
 }
